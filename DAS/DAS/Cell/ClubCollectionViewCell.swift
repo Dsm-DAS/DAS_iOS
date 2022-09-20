5c304418f182077aa598dac9e567de83447b0235
juyeong <juyeongno1@naver.com>
2022-09-20 11:30:35 +0900

    동아리 cell

--- DAS/DAS/Cell/ClubCollectionViewCell.swift
+++ DAS/DAS/Cell/ClubCollectionViewCell.swift
@@ -17,8 +17,18 @@ class ClubCollectionViewCell: UICollectionViewCell {
         $0.text = "동아리이름"
         $0.font = UIFont.boldSystemFont(ofSize: 20)
     }
+    let centerLine = UILabel().then {
+        $0.backgroundColor = .gray
+    }
+    let tagLabel = UILabel().then {
+        $0.text = "#자바스크립트 #스프링부트 #리엑트네이티브 #안드로이드"
+        $0.font = UIFont.boldSystemFont(ofSize: 16)
+        $0.numberOfLines = 2
+    }
+  
     override func layoutSubviews() {
-        [clubImageView, clubNameLabel].forEach {self.addSubview($0)}
+        self.backgroundColor = UIColor(named: "CellBackGround")
+        [clubImageView, clubNameLabel, centerLine, tagLabel].forEach {self.addSubview($0)}
         clubImageView.snp.makeConstraints {
             $0.left.equalToSuperview().inset(8)
             $0.top.equalToSuperview().inset(8)
@@ -27,8 +37,16 @@ class ClubCollectionViewCell: UICollectionViewCell {
         clubNameLabel.snp.makeConstraints {
             $0.left.equalTo(clubImageView.snp.right).offset(8)
             $0.top.equalToSuperview().inset(21)
-            $0.width.equalTo(88)
             $0.height.equalTo(22)
+        }
+        centerLine.snp.makeConstraints {
+            $0.top.equalTo(clubImageView.snp.bottom).offset(8)
+            $0.height.equalTo(1)
+            $0.left.right.equalToSuperview().inset(8)
+        }
+        tagLabel.snp.makeConstraints {
+            $0.top.equalTo(centerLine.snp.bottom).offset(7)
+            $0.left.right.bottom.equalToSuperview().inset(5)
         }
     }
 }
