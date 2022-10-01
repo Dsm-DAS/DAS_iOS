13f2cceb9c6aecd7bf0404a0c34ad72821cc0c06
juyeong <juyeongno1@naver.com>
2022-10-02 00:26:07 +0900

    🌧 :: tabBar뷰 수정

--- DAS/DAS/VC/TabBarViewController.swift
+++ DAS/DAS/VC/TabBarViewController.swift
@@ -1,84 +1,3 @@
-//import UIKit
-//import SnapKit
-//import Then
-//
-//class TabBarViewController: UITabBarController {
-//
-//    var defaultIndex = 0 {
-//        didSet {
-//            self.selectedIndex = defaultIndex
-//        }
-//    }
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        self.view.backgroundColor = .white
-//        self.selectedIndex = defaultIndex
-//    }
-//}
-//extension TabBarViewController {
-//
-//    override func viewWillAppear(_ animated: Bool) {
-//        super.viewWillAppear(animated)
-//
-//        let firstNavigationController = UINavigationController()
-//        let firstTabController = MainViewController()
-//        firstNavigationController.addChild(firstTabController)
-//        ///기본으로 보여질 이미지
-//        firstNavigationController.tabBarItem.image = UIImage(named: "Main")
-//        ///선택되었을 때 보여질 이미지
-//        firstNavigationController.tabBarItem.selectedImage = UIImage(named: "Main")
-//        ///탭바 아이템 타이틀
-//        firstNavigationController.tabBarItem.title = "홈"
-//
-//        let secondNavigationController = UINavigationController()
-//        let secondTabController = ListViewController()
-//        secondNavigationController.addChild(secondTabController)
-//        ///기본으로 보여질 이미지
-//        secondNavigationController.tabBarItem.image = UIImage(named: "List")
-//
-//        ///선택되었을 때 보여질 이미지
-//        secondNavigationController.tabBarItem.selectedImage = UIImage(named: "List")
-//        ///탭바 아이템 타이틀
-//        secondNavigationController.tabBarItem.title = "글"
-//
-//
-//        let thirdNavigationController = UINavigationController()
-//        let thirdTabController = SearchViewController()
-//        thirdNavigationController.addChild(thirdTabController)
-//        ///기본으로 보여질 이미지
-//        thirdNavigationController.tabBarItem.image = UIImage(named: "Search")
-//        ///선택되었을 때 보여질 이미지
-//        thirdNavigationController.tabBarItem.selectedImage = UIImage(named: "Search")
-//        ///탭바 아이템 타이틀
-//        thirdNavigationController.tabBarItem.title = "검색"
-//
-//
-//        let fourthNavigationController = UINavigationController()
-//        let fourthTabController = MenuViewController()
-//        fourthNavigationController.addChild(fourthTabController)
-//        ///기본으로 보여질 이미지
-//        fourthNavigationController.tabBarItem.image = UIImage(named: "Menu")
-//        ///선택되었을 때 보여질 이미지
-//        fourthNavigationController.tabBarItem.selectedImage = UIImage(named: "Menu")
-//        ///탭바 아이템 타이틀
-//        fourthNavigationController.tabBarItem.title = "메뉴"
-//
-//        let tabBar: UITabBar = self.tabBar
-//        tabBar.backgroundColor = UIColor(named: "TabBarBackGround")
-//        tabBar.barTintColor = UIColor.white
-//        ///선택되었을 때 타이틀 컬러
-//        tabBar.tintColor = UIColor(named: "MainColor")
-//        ///선택안된거 타이틀 컬러
-//        tabBar.unselectedItemTintColor = UIColor.lightGray
-//        tabBar.isHidden = false
-//        self.hidesBottomBarWhenPushed = false
-//
-//        let viewControllers = [firstNavigationController, secondNavigationController, thirdNavigationController, fourthNavigationController]
-//        self.setViewControllers(viewControllers, animated: true)
-//    }
-//
-//}
-
 import UIKit
 
 class TabBarViewController : UITabBarController, UITabBarControllerDelegate {
@@ -86,20 +5,18 @@ class TabBarViewController : UITabBarController, UITabBarControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.delegate = self
-
-        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
         
-        let tabOne = UINavigationController(rootViewController: MainViewController()) // 네비게이션 컨트롤러 없는 뷰컨트롤러
-        //탭바를 아름답게 꾸며주겠습니다. 타이틀도 넣어주고 이미지도 넣어줍니다.
+        let tabOne = UINavigationController(rootViewController: MainViewController())
+        
         let tabOneBarItem = UITabBarItem(title: "홈", image: UIImage(named: "Main"), tag: 0)
         tabOne.tabBarItem = tabOneBarItem
         
-        let tabTwo = UINavigationController(rootViewController: ListViewController()) // 뷰컨 품은 네비게이션 컨트롤러
+        let tabTwo = UINavigationController(rootViewController: ListViewController())
         let tabTwoBarItem = UITabBarItem(title: "글", image: UIImage(named: "List"), tag: 1)
         tabTwo.tabBarItem = tabTwoBarItem
         
@@ -108,7 +25,7 @@ class TabBarViewController : UITabBarController, UITabBarControllerDelegate {
         tabThree.tabBarItem = tabThreeBarItem
         
         let tabFour = UINavigationController(rootViewController: MenuViewController())
-        let tabFourBarItem = UITabBarItem(title: "메뉴", image: UIImage(named: "Menu"), tag: 3)
+        let tabFourBarItem = UITabBarItem(title: "내 계정", image: UIImage(named: "Menu"), tag: 3)
         tabFour.tabBarItem = tabFourBarItem
         
         
@@ -116,15 +33,14 @@ class TabBarViewController : UITabBarController, UITabBarControllerDelegate {
         tabBar.backgroundColor = UIColor(named: "TabBarBackGround")
         tabBar.barTintColor = UIColor.white
         tabBar.tintColor = UIColor(named: "MainColor")
-        
         tabBar.unselectedItemTintColor = UIColor.lightGray
         tabBar.isHidden = false
         self.hidesBottomBarWhenPushed = false
-
         self.viewControllers = [tabOne, tabTwo, tabThree, tabFour]
         
     }
-    
-   
-
+    override func viewWillLayoutSubviews() {
+        let kBarHeight =  tabBar.frame.size.height
+        tabBar.frame.origin.y = view.frame.height - kBarHeight
+    }
 }
