4ff82c724a64eb0270c06bb6d9c44e5ef8b06a84
juyeong <juyeongno1@naver.com>
2022-10-23 19:30:15 +0900

    🌧 ::  Service 추가

--- DAS/DAS/Sources/HttpClient/Service.swift
+++ DAS/DAS/Sources/HttpClient/Service.swift
@@ -8,7 +8,7 @@ final class Service {
     
     let provider = MoyaProvider<API>()
     
-    func signIn(_ email: String, _ password: String) -> Single<networkingResult> {
+    func login(_ email: String, _ password: String) -> Single<networkingResult> {
         return provider.rx.request(.login(email: email, password: password))
             .filterSuccessfulStatusCodes()
             .map(TokenModel.self)
@@ -19,6 +19,25 @@ final class Service {
             }
             .catch {[unowned self] in return .just(setNetworkError($0))}
     }
+    func sentEmailCode(_ email: String) -> Single<networkingResult> {
+        return provider.rx.request(.sentEmailCode(email: email))
+            .filterSuccessfulStatusCodes()
+            .map{ response -> networkingResult in
+                return .createOk
+            }
+            .catch {[unowned self] in return .just(setNetworkError($0))}
+    }
+    
+    func codeCheck(_ email: String, _ checkCode: String) -> Single<networkingResult> {
+        return provider.rx.request(.codecheck(email: email, emailCode: checkCode))
+            .filterSuccessfulStatusCodes()
+            .map{ response -> networkingResult in
+                return .createOk
+            }
+            .catch {[unowned self] in return .just(setNetworkError($0))}
+    }
+    
+
     
     func setNetworkError(_ error: Error) -> networkingResult {
             print(error)
