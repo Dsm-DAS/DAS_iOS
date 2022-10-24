aa6a9a577e3ffe43eb330b1c90878ca3802cac34
juyeong <juyeongno1@naver.com>
2022-10-24 22:42:46 +0900

    🌧 :: SignUpViewModel

--- DAS/DAS/Sources/Scene/Login&SignUp VC/SignUpViewModel.swift
+++ DAS/DAS/Sources/Scene/Login&SignUp VC/SignUpViewModel.swift
@@ -8,15 +8,20 @@ class SignUpViewModel: ViewModel {
     struct Input {
         let emailText: Driver<String>
         let codeText: Driver<String>
-//        let nameText: Driver<String>
+        let nameText: Driver<String>
         let passwordText: Driver<String>
+        let gradeText: Driver<String>
+        let classText: Driver<String>
+        let numberText: Driver<String>
         let emailCheckCodeButtonDidTap: Signal<Void>
         let emailCheckButtonDidTap: Signal<Void>
+        let signupBttonDidTap: Signal<Void>
     }
     struct Output {
         let sentEmailCodeResult : PublishRelay<Bool>
         let codeCheckResult : PublishRelay<Bool>
         let signUpResult : PublishRelay<Bool>
+        let isEnable : Driver<Bool>
     }
     private let disposeBag = DisposeBag()
     
@@ -24,9 +29,11 @@ class SignUpViewModel: ViewModel {
         let api = Service()
         let sentEmailCode = input.emailText
         let checkCode = Driver.combineLatest(input.emailText, input.codeText)
+        let signup = Driver.combineLatest(input.emailText, input.nameText, input.passwordText, input.gradeText, input.classText, input.numberText)
         let sentEmailCodeResult = PublishRelay<Bool>()
         let codeCheckResult = PublishRelay<Bool>()
         let signUpResult = PublishRelay<Bool>()
+        let isEnable = signup.map { !$0.0.isEmpty && !$0.1.isEmpty && !$0.2.isEmpty && !$0.3.isEmpty && !$0.4.isEmpty && !$0.5.isEmpty}
         input.emailCheckCodeButtonDidTap
             .asObservable()
             .withLatestFrom(sentEmailCode)
@@ -54,6 +61,19 @@ class SignUpViewModel: ViewModel {
                     codeCheckResult.accept(false)
                 }
             }).disposed(by: disposeBag)
-        return Output(sentEmailCodeResult: sentEmailCodeResult, codeCheckResult: codeCheckResult, signUpResult: signUpResult)
+        input.signupBttonDidTap
+            .asObservable()
+            .withLatestFrom(signup)
+            .flatMap{ email, name, password, grade, classNum, number in
+                api.signup(email, name, password, Int(grade)!, Int(classNum)!, Int(number)!)
+            }.subscribe(onNext: { res in
+                switch res {
+                case .createOk:
+                    signUpResult.accept(true)
+                default:
+                    signUpResult.accept(false)
+                }
+            }).disposed(by: disposeBag)
+        return Output(sentEmailCodeResult: sentEmailCodeResult, codeCheckResult: codeCheckResult, signUpResult: signUpResult, isEnable: isEnable.asDriver())
     }
 }
