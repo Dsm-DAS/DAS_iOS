919338aa4b5324f23945099f826d9909624ea8d8
juyeong <juyeongno1@naver.com>
2022-11-23 11:55:53 +0900

    🌧 :: EmailCode 추가

--- DAS/DAS/Sources/Scene/Login&SignUp VC/EmailCodeViewController.swift
+++ DAS/DAS/Sources/Scene/Login&SignUp VC/EmailCodeViewController.swift
@@ -4,8 +4,9 @@ import Then
 import RxSwift
 import RxCocoa
 class EmailCodeViewController: BaseVC {
+    var email = ""
     private var limitTime: Int = 300
-    let viewModel = SignUpViewModel()
+    let viewModel = EmailCodeViewModel()
     private let joinLabel = UILabel().then {
         $0.text = "가입"
         $0.font = .systemFont(ofSize: 24, weight: .bold)
@@ -26,60 +27,88 @@ class EmailCodeViewController: BaseVC {
         $0.font = .systemFont(ofSize: 16, weight: .bold)
         $0.textColor = UIColor(named: "TextColor")
     }
-    private let emailTextField = UITextField().then {
+    private let codeTextField = UITextField().then {
         $0.addLeftPadding()
         $0.backgroundColor = UIColor(named: "BackGroundColor")
         $0.layer.cornerRadius = 8
         $0.layer.borderWidth = 1
         $0.layer.borderColor = UIColor(named: "SignUpButtonColor")?.cgColor
     }
-    private let nextButton = UIButton().then {
+    private let timerLabel = UILabel().then {
+        $0.text = "05:00"
+        $0.textColor = UIColor(named: "MainColor")
+        $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
+    private let emailCodeImageView = UIImageView().then {
+        $0.image = UIImage(named: "EmailCodeImage")
+    }
+    private let checkButton = UIButton().then {
         $0.backgroundColor = UIColor(named: "MainColor")
-        $0.setTitle("다음", for: .normal)
+        $0.setTitle("인증", for: .normal)
         $0.setTitleColor(UIColor.white, for: .normal)
         $0.layer.cornerRadius = 8
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)    }
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
+    override func bind() {
+        let input = EmailCodeViewModel.Input(codeText: codeTextField.rx.text.orEmpty.asDriver(),
+                                             emailText: email,
+                                             checkButtonDidTap: checkButton.rx.tap.asSignal())
+        let output = viewModel.transform(input)
+        output.result.subscribe(onNext: { [self] in
+            switch $0 {
+            case true:
+                print("코드 맞음")
+                let vc = PasswordViewController()
+                vc.email = email
+                vc.code = codeTextField.text!
+                navigationController?.pushViewController(vc, animated: true)
+            case false:
+                print("코드 다름")
+            }
+        }).disposed(by: disposeBag)
+    }
     override func configureVC() {
-        emailTextField.rx.text.orEmpty
+        self.navigationItem.hidesBackButton = true
+        getSecTime()
+        codeTextField.rx.text.orEmpty
             .map { !$0.isEmpty }
             .subscribe(onNext: {
-                self.nextButton.isEnabled = $0
+                self.checkButton.isEnabled = $0
                 switch $0{
                 case true:
-                    self.nextButton.backgroundColor = UIColor(named: "MainColor")
+                    self.checkButton.backgroundColor = UIColor(named: "MainColor")
                 case false:
-                    self.nextButton.backgroundColor = UIColor(named: "SignUpButton")
+                    self.checkButton.backgroundColor = UIColor(named: "SignUpButton")
                 }
             }).disposed(by: disposeBag)
-        nextButton.rx.tap
-            .subscribe(onNext: {
-                self.viewModel.emailText = self.emailTextField.text ?? ""
-                let vc = EmailCodeViewController()
-                self.navigationController?.pushViewController(vc, animated: true)
-            }).disposed(by: disposeBag)
     }
-//    private func alert(title:String,message:String){
-//        let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
-//        let okAction = UIAlertAction(title: "OK", style: .default) { (action) in
-//            switch title {
-//            case "회원가입":
-//                self.dismiss(animated: true)
-//            default:
-//                break
-//            }
-//        }
-//        alert.addAction(okAction)
-//        present(alert, animated: true, completion:nil)
-//    }
+    @objc func getSecTime(){
+        secToTime(sec: limitTime)
+        limitTime -= 1;
+    }
+    func secToTime(sec: Int) {
+        let minute = (sec % 3600) / 60
+        let second = (sec % 3600) % 60
+        if second < 10 {
+            timerLabel.text = "0\(minute)" + ":" + "0\(second)"
+        } else {
+            timerLabel.text = "0\(minute)" + ":" + "\(second)"
+        }
+        if limitTime != 0 {
+            perform(#selector(getSecTime), with: nil, afterDelay: 1.0)
+        }
+    }
     override func addView() {
         [
             joinLabel,
             emailCodeNoticeLabel,
             CodeStateLabel,
             emailLabel,
-            emailTextField,
-            nextButton
+            codeTextField,
+            emailCodeImageView,
+            checkButton
         ].forEach { view.addSubview($0) }
+        codeTextField.addSubview(timerLabel)
     }
     override func setLayout() {
         joinLabel.snp.makeConstraints {
@@ -104,12 +133,20 @@ class EmailCodeViewController: BaseVC {
             $0.width.equalTo(60)
             $0.height.equalTo(20)
         }
-        emailTextField.snp.makeConstraints {
+        codeTextField.snp.makeConstraints {
             $0.top.equalTo(emailLabel.snp.bottom).offset(4)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(44)
         }
-        nextButton.snp.makeConstraints {
+        timerLabel.snp.makeConstraints {
+            $0.trailing.equalToSuperview().inset(12)
+            $0.centerY.equalToSuperview()
+        }
+        emailCodeImageView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(codeTextField.snp.bottom).offset(64)
+        }
+        checkButton.snp.makeConstraints {
             $0.bottom.equalToSuperview().inset(34)
             $0.leading.trailing.equalToSuperview().inset(16)
             $0.height.equalTo(41)
