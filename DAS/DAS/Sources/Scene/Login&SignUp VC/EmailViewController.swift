21c0f45066a55dfe7dd560f497386d04eee5cf70
juyeong <juyeongno1@naver.com>
2022-11-23 11:56:13 +0900

    🌧 :: EmailVC 추가

--- DAS/DAS/Sources/Scene/Login&SignUp VC/EmailViewController.swift
+++ DAS/DAS/Sources/Scene/Login&SignUp VC/EmailViewController.swift
@@ -4,10 +4,7 @@ import Then
 import RxSwift
 import RxCocoa
 class EmailViewController: BaseVC {
-    let viewModel = SignUpViewModel()
-//    var gradeList = [["1","2","3"],["1","2","3","4"],["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15","16","17","18","19","20"]]
-//    var seletedPicker = ["1","1","1"]
-//    let picker = UIPickerView()
+    let viewModel = EmailViewModel()
     private let joinLabel = UILabel().then {
         $0.text = "가입"
         $0.font = .systemFont(ofSize: 24, weight: .bold)
@@ -39,24 +36,36 @@ class EmailViewController: BaseVC {
         $0.setTitle("다음", for: .normal)
         $0.setTitleColor(UIColor.white, for: .normal)
         $0.layer.cornerRadius = 8
-        $0.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)    }
+        $0.titleLabel?.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+    }
+    override func bind() {
+        let input = EmailViewModel.Input(emailText: emailTextField.rx.text.orEmpty.asDriver(),
+                             nextButtonDidTap: nextButton.rx.tap.asSignal())
+        let output = viewModel.transform(input)
+        output.result.subscribe(onNext: { [self] in
+            switch $0 {
+            case true:
+                print("코드 보냄")
+                let vc = EmailCodeViewController()
+                vc.email = emailTextField.text!
+                print(vc.email)
+                navigationController?.pushViewController(vc, animated: true)
+            case false:
+                print("코드 보내기 실패")
+            }
+        }).disposed(by: disposeBag)
+    }
     override func configureVC() {
         emailTextField.rx.text.orEmpty
             .map { !$0.isEmpty }
-            .subscribe(onNext: {
-                self.nextButton.isEnabled = $0
+            .subscribe(onNext: { [self] in
+                nextButton.isEnabled = $0
                 switch $0{
                 case true:
-                    self.nextButton.backgroundColor = UIColor(named: "MainColor")
+                    nextButton.backgroundColor = UIColor(named: "MainColor")
                 case false:
-                    self.nextButton.backgroundColor = UIColor(named: "SignUpButton")
+                    nextButton.backgroundColor = UIColor(named: "SignUpButton")
                 }
-            }).disposed(by: disposeBag)
-        nextButton.rx.tap
-            .subscribe(onNext: {
-                self.viewModel.emailText = self.emailTextField.text ?? ""
-                let vc = EmailCodeViewController()
-                self.navigationController?.pushViewController(vc, animated: true)
             }).disposed(by: disposeBag)
     }
 //    private func alert(title:String,message:String){
@@ -117,80 +126,3 @@ class EmailViewController: BaseVC {
         }
     }
 }
-//extension SignUpViewController: UIPickerViewDelegate, UIPickerViewDataSource {
-//
-//    func configPickerView() {
-//        picker.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: 216)
-//        picker.delegate = self
-//        picker.dataSource = self
-//
-//        gradeTextField.inputView = picker
-//        classTextField.inputView = picker
-//        numberTextField.inputView = picker
-//        configToolbar()
-//    }
-//    func configToolbar() {
-//        let toolBar = UIToolbar()
-//        toolBar.barStyle = UIBarStyle.default
-//        toolBar.isTranslucent = true
-//        toolBar.tintColor = UIColor.black
-//        toolBar.sizeToFit()
-//
-//        let doneBT = UIBarButtonItem(title: "완료", style: .plain, target: self, action: #selector(self.donePicker))
-//        let flexibleSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-//        let cancelBT = UIBarButtonItem(title: "취소", style: .plain, target: self, action: #selector(self.cancelPicker))
-//
-//        // 만든 아이템들을 세팅해주고
-//        toolBar.setItems([cancelBT,flexibleSpace,doneBT], animated: false)
-//        toolBar.isUserInteractionEnabled = true
-//
-//        // 악세사리로 추가한다.
-//        gradeTextField.inputAccessoryView = toolBar
-//        classTextField.inputAccessoryView = toolBar
-//        numberTextField.inputAccessoryView = toolBar
-//    }
-//    @objc func donePicker() {
-////        let row = self.picker.selectedRow(inComponent: 0)
-////        self.picker.selectRow(row, inComponent: 0, animated: false)
-//        self.gradeTextField.resignFirstResponder()
-//        self.classTextField.resignFirstResponder()
-//        self.numberTextField.resignFirstResponder()
-//    }
-//
-//    // "취소" 클릭 시 textfield의 텍스트 값을 nil로 처리 후 입력창 내리기
-//    @objc func cancelPicker() {
-//        self.gradeTextField.text = nil
-//        self.classTextField.text = nil
-//        self.numberTextField.text = nil
-//        self.gradeTextField.resignFirstResponder()
-//        self.classTextField.resignFirstResponder()
-//        self.numberTextField.resignFirstResponder()
-//    }
-//    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-//        return gradeList.count
-//    }
-//    // pickerview의 선택지는 데이터의 개수만큼
-//    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-//        return gradeList[component].count
-//    }
-//    // pickerview 내 선택지의 값들을 원하는 데이터로 채워준다.
-//    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-//        return gradeList[component][row]
-//    }
-//    // textfield의 텍스트에 pickerview에서 선택한 값을 넣어준다.
-//    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-//        switch component {
-//        case 0:
-//            seletedPicker[0] = gradeList[component][row]
-//            gradeTextField.text = gradeList[component][row]+"학년"
-//        case 1:
-//            seletedPicker[1] = gradeList[component][row]
-//            classTextField.text = gradeList[component][row]+"반"
-//        case 2:
-//            seletedPicker[2] = gradeList[component][row]
-//            numberTextField.text = gradeList[component][row]+"번"
-//        default:
-//            print("Error!!")
-//        }
-//    }
-//}
---
+++ DAS/DAS/Sources/Scene/Login&SignUp VC/EmailViewModel.swift
@@ -0,0 +1,33 @@
+import Foundation
+import RxSwift
+import RxCocoa
+
+class EmailViewModel: ViewModel {
+    struct Input {
+        let emailText: Driver<String>
+        let nextButtonDidTap: Signal<Void>
+    }
+    struct Output {
+        let result : PublishRelay<Bool>
+    }
+    private let disposeBag = DisposeBag()
+    
+    func transform(_ input: Input) -> Output {
+        let api = Service()
+        let result = PublishRelay<Bool>()
+        input.nextButtonDidTap
+            .asObservable()
+            .withLatestFrom(input.emailText)
+            .flatMap{ email in
+                api.sendEmailCode(email)
+            }.subscribe(onNext: { res in
+                switch res {
+                case .createOk:
+                    result.accept(true)
+                default:
+                    result.accept(false)
+                }
+            }).disposed(by: disposeBag)
+        return Output(result: result)
+    }
+}
