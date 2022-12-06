437792ab6baa07b1d584f49b2bf3bcf95b8d2367
doyeon <parkdoyeon3239@gmail.com>
2022-12-06 20:25:37 +0900

    🌧 :: 학생리스트 뷰 추가

---
+++ DAS/DAS/Sources/Cell/StudentFilterCollectionViewCell.swift
@@ -0,0 +1,30 @@
+//
+//  StudentFilterCollectionViewCell.swift
+//  DAS
+//
+//  Created by 박도연 on 2022/12/04.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class StudentFilterCollectionViewCell: UICollectionViewCell {
+    let nameLabel = UILabel().then {
+        $0.text = ""
+        $0.font = .systemFont(ofSize: 16, weight: .regular)
+        $0.textColor = .black
+    }
+    override func layoutSubviews() {
+        self.layer.cornerRadius = 18
+        self.layer.borderWidth = 1
+        self.layer.borderColor = UIColor(named: "SignUpButtonColor")?.cgColor
+        
+        contentView.addSubview(nameLabel)
+        
+        nameLabel.snp.makeConstraints {
+            $0.leading.equalToSuperview().inset(12)
+            $0.top.bottom.equalToSuperview().inset(8)
+        }
+    }
+}
